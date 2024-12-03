2d5569a30026e0f85467729eaafeb29e89d4e362
geociencias98 <geociencias.cr@gmail.com>
2024-12-03 13:03:51 -0600

    Update acg.py

--- acg.py
+++ acg.py
@@ -22,8 +22,14 @@ st.subheader("Base de datos")
 st.write(data)
 
 # URL de la imagen
+image_url = "https://raw.githubusercontent.com/geociencias98/Proyecto/main/Grafico.png"
+
+# Mostrar la imagen en Streamlit
+st.image(image_url, caption='Gráfico', use_column_width=True)
+
+# URL de la imagen
 image_url = "https://raw.githubusercontent.com/geociencias98/Proyecto/main/Mapa.png"
 
 # Mostrar la imagen en Streamlit
-st.image(image_url, caption='Mapa', use_column_width=True)
+st.image(image_url, caption='Mapa de ubicación de avistamientos de mamíferos en el ACG', use_column_width=True)
 
