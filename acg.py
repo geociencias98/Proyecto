4af2222219efef802607045ee3c22588f00420e2
geociencias98 <geociencias.cr@gmail.com>
2024-12-03 11:32:53 -0600

    Update acg.py

--- acg.py
+++ acg.py
@@ -24,19 +24,6 @@ st.write(data)
 # Normalizar nombres de columnas
 data.columns = data.columns.str.strip().str.lower()
 
-# Verificar que la columna 'common_name' exista
-if 'common_name' in data.columns:
-    # Crear un selectbox para el campo 'common_name'
-    common_name = data['common_name'].unique()
-    selected_common_name = st.selectbox("Selecciona un nombre común:", common_name)
-
-    # Filtrar los datos según el nombre común seleccionado
-    filtered_data = data[data['common_name'] == selected_common_name]
-
-    # Mostrar tabla con los datos filtrados
-    st.subheader("Datos Filtrados por Nombre Común")
-    st.dataframe(filtered_data)
-else:
-    st.write("La columna 'common_name' no se encuentra en el DataFrame.")
-
-
+# Mostrar la tabla
+st.subheader('Casos totales y muertes totales por país y por día')
+st.dataframe(data, hide_index=True)
